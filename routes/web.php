269a222f5a9ceceee75e710ea4d9f08ffdf43635
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-31 20:05:19 +0200

    crud routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -19,10 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [GuestHomeController::class, 'index'])->name('guest.home');
 
 
-// All authenthicated admin routes here
+// All protected Admin routes here
 Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function(){
     Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+    Route::resource('projects', ProjectController::class);
 });
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
