b5ad7cf06560952521b6a5fbda39a59d52a232cb
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-31 17:54:43 +0200

    creating and updating seeders, models and migratio

--- database/migrations/2023_08_31_142915_create_projects_table.php
+++ database/migrations/2023_08_31_142915_create_projects_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50); // Project Name
+            $table->string('github_url'); // GitHub URL
+            $table->string('screenshot_path')->nullable(); // Screenshot File Path
+            $table->text('description')->nullable(); // Project Description
+            $table->string('technologies')->nullable(); // Technologies used
+            $table->boolean('is_featured')->default(false); // Flag to identify if the project should be highlighted
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 1; $i <= 10; $i++) {
+            $project = new Project();
+            $project->name = $faker->word();
+            $project->github_url = $faker->url(); 
+            $project->screenshot_path = $faker->imageUrl(640, 480);
+            $project->description = $faker->paragraph();
+            $project->technologies = $faker->currencyCode(); 
+            $project->is_featured = $faker->boolean();
+            $project->save();
+        }
     }
 }
