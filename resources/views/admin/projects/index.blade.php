4642defef596457fb4c5a34e4045da0ac3d033c6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-01 15:07:34 +0200

    index modified

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,17 +7,16 @@
   <h1 class="py-4">Projects</h1>
 </nav>
 <div class="card py-5 px-3">
-  <table class="table table-striped">
+  <table class="table table-borderless ">
     <thead>
       <tr>
         <th scope="col">#</th>
         <th scope="col">Name</th>
         <th scope="col">URL</th>
-        <th scope="col">Screenshot</th>
-        <th scope="col">Description</th>
-        <th scope="col">Languages</th>
+        <th scope="col">Created At</th>
+        <th scope="col">Updated At</th>
         <th scope="col">Featured</th>
-        <th></th>
+        <th scope="col" class="text-center"> Actions</th>
       </tr>
     </thead>
     <tbody>
@@ -26,10 +25,24 @@
         <th scope="row">{{$project->id}}</th>
         <td>{{$project->name}}</td>
         <td>{{$project->github_url}}</td>
-        <td>{{$project->screenshot_path}}</td>
-        <td>{{$project->description}}</td>
-        <td>{{$project->technologies}}</td>
+        <td>{{$project->created_at}}</td>
+        <td>{{$project->updated_at}}</td>
         <td>{{$project->is_featured}}</td>
+        <td class="d-flex align-items-center ">
+          <a href="{{route ('admin.projects.show', $project)}}" class="btn btn-sm btn-primary">
+            <i class="fa-solid fa-eye"></i>
+          </a>
+          <a href="{{route ('admin.projects.edit', $project)}}" class="btn btn-sm btn-warning ms-2">
+            <i class="fa-solid fa-pencil"></i>
+          </a>
+          <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-sm btn-danger ms-2">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+          </form>
+        </td>
         @empty
             <h3 class="text-center">No Projects in here! Get to work!</h3>
         @endforelse
