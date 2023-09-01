29ff20975d73fcb0fb0c51418ae3d72675d275c2
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-01 15:07:22 +0200

    project seeder updated

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -16,8 +16,8 @@ class ProjectSeeder extends Seeder
         for ($i = 1; $i <= 10; $i++) {
             $project = new Project();
             $project->name = $faker->word();
-            $project->github_url = $faker->url(); 
-            $project->screenshot_path = $faker->imageUrl(640, 480);
+            $project->github_url = url('https://github.com/'); 
+            $project->screenshot_path = $faker->imageUrl(640, 480, 'animals', true);
             $project->description = $faker->paragraph();
             $project->technologies = $faker->currencyCode(); 
             $project->is_featured = $faker->boolean();
