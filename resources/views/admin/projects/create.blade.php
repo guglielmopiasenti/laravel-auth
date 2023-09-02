f89510058e34e056c777c6404db38726d3358ceb
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-02 18:39:23 +0200

    add display validation error section + old method

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -11,31 +11,42 @@
                     Create Project
                 </div>
                 <div class="card-body">
+                    <!-- Display Validation Errors -->
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Project Name</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+                            <textarea class="form-control" id="description" name="description" rows="3" required>{{ old('description') }}</textarea>
                         </div>
                         <div class="mb-3">
                             <label for="technologies" class="form-label">Technologies</label>
-                            <input type="text" class="form-control" id="technologies" name="technologies" required>
+                            <input type="text" class="form-control" id="technologies" name="technologies" value="{{ old('technologies') }}" required>
                         </div>
                         <div class="mb-3">
                             <label for="screenshot" class="form-label">Screenshot</label>
                             <input type="file" class="form-control" id="screenshot" name="screenshot">
                         </div>
                         <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="is_featured" name="is_featured">
+                            <input type="checkbox" class="form-check-input" id="is_featured" name="is_featured" {{ old('is_featured') ? 'checked' : '' }}>
                             <label class="form-check-label" for="is_featured">Is Featured?</label>
                         </div>
                         <div class="mb-3">
                             <label for="github_url" class="form-label">GitHub URL</label>
-                            <input type="url" class="form-control" id="github_url" name="github_url">
+                            <input type="url" class="form-control" id="github_url" name="github_url" value="{{ old('github_url') }}">
                         </div>
                         <button type="submit" class="btn btn-primary">Create Project</button>
                     </form>
